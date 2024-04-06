83849136c8b4248c3ae904fdd85562d8e5a6d79a
Ashish Zarkar <azupworkuser@gmail.com>
2024-04-06 18:50:54 +0530

    Show success and error messages

---
+++ resources/views/partial/successMessage.blade.php
@@ -0,0 +1,11 @@
+@if(session('success'))
+	<div class="alert alert-success mt-3" id="alert" roles="alert">
+		{{ session('success') }} 
+	</div> 
+@endif  
+
+@if(session('danger'))
+	<div class="alert alert-danger mt-3" id="alert" roles="alert">
+		{{ session('danger') }} 
+	</div> 
+@endif  
